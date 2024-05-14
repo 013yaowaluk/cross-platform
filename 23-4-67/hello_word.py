01d4a5fe51bde2ec986ccd714603c40bddee757d
013yaowaluk <366347221013-st@rmutsb.ac.th>
2024-05-14 09:04:06 +0700

    Add files via upload

---
+++ 23-4-67/hello_word.py
@@ -0,0 +1,11 @@
+from kivy.app import App
+from kivy.uix.label import Label
+
+class HelloWorldApp(App):
+
+    def build(self):
+        return
+
+if __name__ == '__main__':
+
+    HelloWorldApp().run()
